5e355aa155bd2569374c927900a0a09eb424a3b2
Babu-Vadaboina <tatababuvadaboina@gmail.com>
2025-05-16 22:32:35 +0530

    BE - added admin route logic

--- Backend/controllers/adminController.js
+++ Backend/controllers/adminController.js
@@ -1,11 +1,44 @@
 const { get } = require("mongoose");
 const { adminModel } = require("../db");
+const jwt = require("jsonwebtoken");
 
-const signin = (req, res) => {
+const signup = async (req, res) => {
+  const { email, password, firstname, lastname } = req.body;
+  try {
+    await adminModel.create({
+      email,
+      password,
+      firstname,
+      lastname,
+    });
+  } catch (err) {
+    console.log("error in admin signup", err.message);
+  }
   res.json({
-    message: "admin signin endpoint here",
+    id: admin._id,
+    message: "admin signup successfully",
   });
 };
+
+const signin = async (req, res) => {
+  const { email, password } = req.body;
+  const admin = await adminModel.findOne({ email, password });
+  if (admin) {
+    // console.log(admin._id);
+    // console.log(admin.email);
+    // console.log(admin.password);
+    const token = jwt.sign({ id: admin._id }, process.env.JWT_ADMIN_TOKEN);
+    res.status(200).json({
+      token: token,
+      message: "admin signed in successfully",
+    });
+  } else {
+    res.status(403).json({
+      message: "invalid credentials",
+    });
+  }
+};
+
 const createCourse = (req, res) => {
   res.json({
     message: "admin create course endpoint",
@@ -24,6 +57,7 @@ const getAllCourses = (req, res) => {
 
 module.exports = {
   signin,
+  signup,
   createCourse,
   updateCourse,
   getAllCourses,
--- Backend/routes/admin.js
+++ Backend/routes/admin.js
@@ -3,6 +3,7 @@ const adminRouter = express.Router();
 const adminController = require("../controllers/adminController");
 
 adminRouter.post("/signin", adminController.signin);
+adminRouter.post("/signup", adminController.signup);
 adminRouter.post("/createCourse", adminController.createCourse);
 adminRouter.put("/updateCourse/:id", adminController.updateCourse);
 adminRouter.get("/allCourses", adminController.getAllCourses);
